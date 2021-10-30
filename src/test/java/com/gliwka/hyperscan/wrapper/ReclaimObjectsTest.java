28f0ce58220358d81f4c7e627bd3aac58f98f890
Milan Mimica <milan.mimica@infobip.com>
2021-10-30 17:56:59 +0200

    proper deallocator

--- src/test/java/com/gliwka/hyperscan/wrapper/ReclaimObjectsTest.java
+++ src/test/java/com/gliwka/hyperscan/wrapper/ReclaimObjectsTest.java
@@ -19,15 +19,15 @@ public class ReclaimObjectsTest {
     public void test() throws CompileErrorException, InterruptedException {
         Database db = Database.compile(new Expression("Te?st"));
         // some reclaimable objects are left behind after compile
-        assertEquals(3, Pointer.totalCount());
+        assertEquals(4, Pointer.totalCount());
 
         try (Scanner scanner = new Scanner()) {
             scanner.allocScratch(db);
-            assertEquals(4, Pointer.totalCount());
+            assertEquals(6, Pointer.totalCount());
             scanner.scan(db, "Test");
-            assertEquals(4, Pointer.totalCount());
+            assertEquals(6, Pointer.totalCount());
         }
-        assertEquals(3, Pointer.totalCount());
+        assertEquals(4, Pointer.totalCount());
 
         WeakReference<Database> ref = new WeakReference<>(db);
         //noinspection UnusedAssignment
