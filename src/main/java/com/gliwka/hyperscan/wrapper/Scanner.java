6586bb7ff983c441c9d4a00c15227bd5ad14670e
Milan Mimica <milan.mimica@infobip.com>
2023-06-16 09:30:17 +0200

    Rewrite scanner to use a scoped handler.

--- src/main/java/com/gliwka/hyperscan/wrapper/Scanner.java
+++ src/main/java/com/gliwka/hyperscan/wrapper/Scanner.java
@@ -20,11 +20,9 @@ import static com.gliwka.hyperscan.jni.hyperscan.*;
 import static java.util.Collections.emptyList;
 
 /**
- * Scanner, can be used with databases to scan for expressions in input string
- * In case of multithreaded scanning, you need one scanner instance per CPU thread.
- *
- * Scanner references native resources. It is paramount to close it after use.
- * There can only be 256 non-closed scanner instances.
+ * Scanner, can be used with databases to scan for expressions in input string In case of multithreaded scanning, you need one scanner instance per CPU thread.
+ * <p>
+ * Scanner references native resources. It is paramount to close it after use. There can only be 256 non-closed scanner instances.
  */
 public class Scanner implements Closeable {
     private static final AtomicInteger count = new AtomicInteger();
@@ -37,13 +35,17 @@ public class Scanner implements Closeable {
         // the right scanner. I've decided against it to keep this implementation simple and to not have
         // to manage references between context pointers and scanner instances
 
-        if(count.get() >= 256) {
+        if (count.get() >= 256) {
             throw new RuntimeException("There can only be 256 non-closed Scanner instances. Create them once per thread!");
         }
 
         count.incrementAndGet();
     }
 
+    public Scanner(Database db) {
+        this();
+        allocScratch(db);
+    }
 
     private static class NativeScratch extends hs_scratch_t {
         void registerDeallocator() {
@@ -56,6 +58,7 @@ public class Scanner implements Closeable {
 
     /**
      * Check if the hardware platform is supported
+     *
      * @return true if supported, otherwise false
      */
     public static boolean getIsValidPlatform() {
@@ -65,6 +68,7 @@ public class Scanner implements Closeable {
 
     /**
      * Get the version information for the underlying hyperscan library
+     *
      * @return version string
      */
     public static String getVersion() {
@@ -73,26 +77,27 @@ public class Scanner implements Closeable {
 
     /**
      * Get the scratch space size in bytes
+     *
      * @return count of bytes
      */
     public long getSize() {
-        if(scratch == null) {
+        if (scratch == null) {
             throw new IllegalStateException("Scratch space has alredy been deallocated");
         }
 
-        try(SizeTPointer size = new SizeTPointer(1)) {
+        try (SizeTPointer size = new SizeTPointer(1)) {
             hs_scratch_size(scratch, size);
             return size.get();
         }
     }
 
     /**
-     * Allocate a scratch space.  Must be called at least once with each
-     * database that will be used before scan is called.
+     * Allocate a scratch space.  Must be called at least once with each database that will be used before scan is called.
+     *
      * @param db Database containing expressions to use for matching
      */
     public void allocScratch(final Database db) {
-        if(scratch == null) {
+        if (scratch == null) {
             throw new IllegalStateException("Scratch space has already been deallocated");
         }
 
@@ -100,73 +105,71 @@ public class Scanner implements Closeable {
         int hsError = hs_alloc_scratch(dbPointer, scratch);
         scratch.registerDeallocator();
 
-        if(hsError != 0) {
+        if (hsError != 0) {
             throw HyperscanException.hsErrorToException(hsError);
         }
     }
 
-    private final LinkedList<long[]> matchedIds = new LinkedList<>();
-
-    private final match_event_handler matchHandler = new match_event_handler() {
-        public int call(int id, long from, long to, int flags, Pointer context) {
-            long[] tuple = {id, from, to};
-            matchedIds.add(tuple);
-            return 0;
-        }
-    };
-
     /**
-     * scan for a match in a string using a compiled expression database
-     * Can only be executed one at a time on a per instance basis
-     * @param db Database containing expressions to use for matching
+     * scan for a match in a string using a compiled expression database Can only be executed one at a time on a per instance basis
+     *
+     * @param db    Database containing expressions to use for matching
      * @param input String to match against
      * @return List of Matches
      */
     public List<Match> scan(final Database db, final String input) {
-        if(scratch == null) {
+        if (scratch == null) {
             throw new IllegalStateException("Scratch space has already been deallocated");
         }
 
-        hs_database_t database = db.getDatabase();
-
-        matchedIds.clear();
-        final byte[] bytes = input.getBytes(StandardCharsets.UTF_8);
-        try (final BytePointer bytePointer = new BytePointer(ByteBuffer.wrap(bytes))) {
-            int hsError = hs_scan(database, bytePointer, bytes.length, 0, scratch, matchHandler, null);
-
-            if (hsError != 0) {
-                throw HyperscanException.hsErrorToException(hsError);
+        final LinkedList<long[]> matchedIds = new LinkedList<>();
+        try (final match_event_handler matchHandler = new match_event_handler() {
+            public int call(int id, long from, long to, int flags, Pointer context) {
+                long[] tuple = {id, from, to};
+                matchedIds.add(tuple);
+                return 0;
             }
-        }
+        }) {
 
-        if(matchedIds.isEmpty()) {
-            return emptyList();
-        }
+            hs_database_t database = db.getDatabase();
 
-        //if string length == byte length, all characters are 1 byte wide -> ASCII, no position mapping necessary
-        if(bytes.length == input.length()) {
-            return processMatches(input, bytes, db, position -> position);
-        }
-        else {
-            final int[] byteToStringPosition = Utf8.byteToStringPositionMap(input, bytes.length);
-            return processMatches(input, bytes, db, bytePosition -> byteToStringPosition[bytePosition]);
+            final byte[] bytes = input.getBytes(StandardCharsets.UTF_8);
+            try (final BytePointer bytePointer = new BytePointer(ByteBuffer.wrap(bytes))) {
+                int hsError = hs_scan(database, bytePointer, bytes.length, 0, scratch, matchHandler, null);
+
+                if (hsError != 0) {
+                    throw HyperscanException.hsErrorToException(hsError);
+                }
+            }
+
+            if (matchedIds.isEmpty()) {
+                return emptyList();
+            }
+
+            //if string length == byte length, all characters are 1 byte wide -> ASCII, no position mapping necessary
+            if (bytes.length == input.length()) {
+                return processMatches(matchedIds, input, bytes, db, position -> position);
+            } else {
+                final int[] byteToStringPosition = Utf8.byteToStringPositionMap(input, bytes.length);
+                return processMatches(matchedIds, input, bytes, db, bytePosition -> byteToStringPosition[bytePosition]);
+            }
         }
     }
 
-    private List<Match> processMatches(String input, byte[] bytes, Database db, Function<Integer, Integer> position) {
+    private List<Match> processMatches(List<long[]> matchedIds, String input, byte[] bytes, Database db, Function<Integer, Integer> position) {
         final LinkedList<Match> matches = new LinkedList<>();
 
-        matchedIds.forEach( tuple -> {
-            int id = (int)tuple[0];
+        matchedIds.forEach(tuple -> {
+            int id = (int) tuple[0];
             long from = tuple[1];
             long to = tuple[2] < 1 ? 1 : tuple[2];
             String match = "";
             Expression matchingExpression = db.getExpression(id);
 
-            int startIndex = position.apply((int)from);
-            int endIndex = position.apply((int)to - 1);
+            int startIndex = position.apply((int) from);
+            int endIndex = position.apply((int) to - 1);
 
-            if(matchingExpression.getFlags().contains(ExpressionFlag.SOM_LEFTMOST)) {
+            if (matchingExpression.getFlags().contains(ExpressionFlag.SOM_LEFTMOST)) {
                 match = input.substring(startIndex, endIndex + 1);
             }
 
@@ -183,7 +186,6 @@ public class Scanner implements Closeable {
     @Override
     public void close() {
         scratch.close();
-        matchHandler.close();
         count.decrementAndGet();
         scratch = null;
     }
